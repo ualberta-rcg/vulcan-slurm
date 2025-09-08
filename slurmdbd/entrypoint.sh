2574d55a3abc5d736eab07c4c7cfd99c8f7655db
Rahim Khoja <rahim@khoja.ca>
2025-09-08 14:08:40 -0700

    Update entrypoint.sh

--- slurmdbd/entrypoint.sh
+++ slurmdbd/entrypoint.sh
@@ -17,6 +17,12 @@ fi
 chown slurm:slurm /etc/slurm/slurmdbd.conf
 chmod 600 /etc/slurm/slurmdbd.conf
 
+# Create required directories for slurmdbd
+mkdir -p /var/spool/slurmctld /var/spool/slurmd /var/spool/slurmdbd /var/spool/slurmrestd /var/log/slurm/ /var/run/slurm /etc/slurm
+touch /var/log/slurm/slurm-dbd.log /var/log/slurm/slurmctld.log /var/spool/slurmctld/priority_last_decay_ran
+chown -R slurm:slurm /var/spool/slurmctld /var/spool/slurmd /var/spool/slurmdbd /var/spool/slurmrestd /var/log/slurm/ /var/run/slurm /etc/slurm
+chmod 755 /var/spool/slurmdbd
+
 # =============================================================================
 # MUNGE SETUP - Standardized across all Slurm services
 # =============================================================================
